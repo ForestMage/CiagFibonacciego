7f74ed988d04805de05cdcb4712e82a98005b388
fores <fores@DESKTOP-JQOCTIM>
2022-10-31 19:07:10 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Serialization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        long x = 0;
+        long y = 1;
+        Console.WriteLine("Wpisz ilość wyników:");
+        int rep = Convert.ToInt32(Console.ReadLine());
+        int n = 1;
+
+        while (n < rep)
+        {
+            for (; x < y;)
+            {
+                n++;
+                Console.WriteLine(x);
+                x = x + y;
+            }
+            for (; x >= y;)
+            {
+                n++;
+                Console.WriteLine(y);
+                y = y + x;
+            }
+
+
+        }  
+    }
+}
